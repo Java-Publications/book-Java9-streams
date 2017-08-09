b0ac026019dd951dd917b0dde3a4dc7c2f44f247
Sven Ruppert <sven.ruppert@gmail.com>
2017-08-09 15:37:03 +1000

    v003

---
+++ src/main/java/org/rapidpm/book/java9/streams/v003/MainA.java
@@ -0,0 +1,48 @@
+package org.rapidpm.book.java9.streams.v003;
+
+import static org.rapidpm.book.java9.streams.utils.DemoData.nextStream;
+
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.rapidpm.book.java9.streams.utils.DemoData;
+import org.rapidpm.frp.model.Pair;
+
+/**
+ *
+ */
+public class MainA {
+
+  public static Stream<Pair<String,String>> nextStream(){
+    return DemoData.nextStreamWithDuplicates();
+  }
+
+
+  public static void main(String[] args) {
+    final Map<String, String> mapA = nextStream()
+        .collect(Collectors.toMap(new Function<Pair<String, String>, String>() {
+          @Override
+          public String apply(Pair<String, String> p) {
+            return p.getT1();
+          }
+        } , new Function<Pair<String, String>, String>() {
+          @Override
+          public String apply(Pair<String, String> p) {
+            return p.getT2().toLowerCase();
+          }
+        }));
+    System.out.println("mapA = " + mapA);
+
+    final Map<String, String> mapB = nextStream()
+        .collect(Collectors.toMap(Pair::getT1 , p -> p.getT2().toLowerCase()));
+
+    System.out.println("mapB = " + mapB);
+
+
+
+
+  }
+}
