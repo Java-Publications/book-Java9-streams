085e345149a48a43a02b526a83b0eb92275ebc16
Sven Ruppert <sven.ruppert@gmail.com>
2017-08-13 12:39:47 +1000

    v016

---
+++ src/main/java/org/rapidpm/book/java9/streams/v016/MainA.java
@@ -0,0 +1,32 @@
+package org.rapidpm.book.java9.streams.v016;
+
+import static java.util.stream.Collectors.filtering;
+import static java.util.stream.Collectors.toList;
+import static org.rapidpm.book.java9.streams.utils.DemoData.nextIntegerStreamWithDuplicates;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ *
+ */
+public class MainA {
+
+  public static void main(String[] args) {
+
+    final List<Integer> resultA = nextIntegerStreamWithDuplicates()
+        .collect(filtering(new Predicate<Integer>() {
+          @Override
+          public boolean test(Integer v) {
+            return v > 5;
+          }
+        } , toList()));
+
+    System.out.println("resultA = " + resultA);
+
+    final List<Integer> resultB = nextIntegerStreamWithDuplicates()
+        .collect(filtering(v -> v > 5 , toList()));
+
+    System.out.println("resultB = " + resultB);
+  }
+}
